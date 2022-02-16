021987487bd5745b3dce9d0975703579d06e1ed4
Hugo Vercellone <hugo.vercelloneg@gmail.com>
2022-02-16 10:01:32 +0100

    4

--- testFile.py
+++ testFile.py
@@ -1,8 +1,8 @@
 hellos = "Hello World"
 
 for hello in hellos:
-    print(hello, end = "  ")
+    print(hello, end = " ")
     
 
 for i in range (10):
-    print (' ' * 9-i / 2, i*'*')
+    print (((9-i)//2)*' ', i*'°')
